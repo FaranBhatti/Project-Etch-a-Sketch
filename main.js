f3b2aad18f3c084fb6370546da811eb825452b7d
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-09-15 20:32:06 -0700

    able to select row and columns for grid. need to make it a bit more dynamic

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-let container = document.querySelector('#container');
+const rows = 16;
+const columns = 10;
 
-console.log(container);
+let gridContainer = document.querySelector('#container');
+
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+        
+        // create a grid item
+        let gridItem = document.createElement('div');
+        
+        // give it it's style
+        gridItem.style.border = 'solid black 0.1px';
+        gridItem.style.width = "50px";
+        gridItem.style.height = "50px";
+        gridContainer.appendChild(gridItem);
+    }   
+}
